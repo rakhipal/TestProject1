3151e1c90cfac501fb60c9f72c69d5224e2dd469
rakhipal <rakhi.pal54@gmail.com>
2021-05-23 16:22:07 +0100

    commit pages

---
+++ Pages/HomePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace TestProject1.Pages
+{
+    class HomePage
+    {
+       
+        [FindsBy(How =How.XPath,Using = "//*[@id='SW']/div[1]/div[2]/div/div/nav/ul/li[2]/a")]
+        public IWebElement hotel { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='hsw_search_button']")]
+        public IWebElement searchButton { get; set; }
+
+
+
+    }
+}
---
+++ Pages/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace TestProject1.Pages
+{
+    class Login
+    {
+        [FindsBy(How = How.XPath, Using = "//*[@id='SW']/div[1]/div[1]/ul/li[5]/div[3]/div/div[2]/div/p/label")]
+        public IWebElement signIn { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='username']")]
+        public IWebElement Enterusername { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='SW']/div[1]/div[2]/div[2]/section/form/div[2]/button")]
+        public IWebElement continueButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='password']")]
+        public IWebElement EnterPassword { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='SW']/div[1]/div[2]/div[2]/section/form/div[2]/button/span")]
+        public IWebElement loginButton { get; set; }
+
+
+
+    }
+}
