160057ec1d5549adcd96a71487ad2e242ad3408a
rakhipal <rakhi.pal54@gmail.com>
2021-05-23 16:12:26 +0100

    Add files via upload

---
+++ Hooks1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace TestProject1.Hooks
+{
+    [Binding]
+    public class Hooks1
+    {
+       
+       // public IWebDriver driver;
+
+        //String TestURL = "https://www.makemytrip.com/";
+       
+    }
+}
