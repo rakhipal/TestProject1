fabe00bc49d6266ebb481500271425557af92c6d
rakhipal <rakhi.pal54@gmail.com>
2021-05-23 16:25:39 +0100

    Add files via upload

---
+++ Base/BaseClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject1.Base
+{
+   public class BaseClass
+    {
+        //public String baseURL = "http://automationpractice.com";
+        //public IWebDriver driver;
+        //public string firstName = "Rakhi";
+       
+       
+    }
+}
